81b07e3f7e057077f72fc3b030f43458aad0c245
ludovicm67 <ludovicmuller1@gmail.com>
2017-02-23 19:11:59 +0100

    clean

--- App.java
+++ App.java
@@ -1,8 +1,7 @@
 // Classe principale
 public class App {
 	public static void main (String [] args) {
-		InFile in = new InFile("./example.in");
+		InFile in = new InFile("./input/me_at_the_zoo.in");
 		in.debug();
-
 	}
 }
