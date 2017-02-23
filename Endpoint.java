96c4d8afad9d4649034de56118c5e8d0e27abc72
Koin <koin@pc-loul.localdomain>
2017-02-23 21:20:36 +0100

    Adds attributes and a method to Endpoint ecks deeeeeeeeeee

--- Endpoint.java
+++ Endpoint.java
@@ -9,8 +9,15 @@ public class Endpoint {
   // ID referencing the endpoint
   private int id;
 
+  // Latency to the data center
+  private int dataLatency;
+
   // List of cache servers
   private ArrayList<Cache> caches;
+
+  // List of latencies for each cache server
+  private ArrayList<Integer> cacheLatencies;
+
 
 
   // Constructor
@@ -18,6 +25,13 @@ public class Endpoint {
   {
     this.id = id;
     this.caches = new ArrayList<Cache>();
+  }
+
+  // Adds a cache and a latency to each respective list
+  public void addCache(Cache c, int l)
+  {
+    this.caches.add(c);
+    this.cacheLatencies.add(l);
   }
 
   public int getId()
